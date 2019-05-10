36642620d13c24000db8157dee4945a51480487e
vibhaskarn <vibhaskk@gmail.com>
2019-05-10 11:13:41 +0530

    Recipe code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('Test Recipe','This test description','https://www.spoonacular.com/whatever/pineapple.jpg')
+  ];
 
   constructor() { }
 
